fb2ee676a047df5fc9d09b419ede58917d5ab48d
davidCañedo <57066973+DavidCV61@users.noreply.github.com>
2023-01-22 21:58:38 -0600

    Update binaryexpoenentiation.cpp

--- Algebra/Binary Exponentiation/binaryexpoenentiation.cpp
+++ Algebra/Binary Exponentiation/binaryexpoenentiation.cpp
@@ -12,7 +12,16 @@ long long binpow(long long a, long long b) {
     else
         return res * res;
 }
-
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
 int main(){
 
 	std::cin.tie(nullptr);
